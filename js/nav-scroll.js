b1586d988c3b4dce9da894543299eb142182db23
DigitalHawk1 <antanaslstanys@gmail.com>
2018-03-26 11:40:04 +0300

    Fixed header, edited works section gallery

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,11 +1,12 @@
-function goToByScroll(id){
-  $('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
-}
-{
+'use strict'
 
-  'use strict'
-
-  $('#header-nav').navScroll({
-    scrollSpy: true,
-  })
+function goToByScroll (id) {
+  $('html,body').animate({scrollTop: $('#' + id).offset().top}, 'slow')
 }
+
+$('#header-nav').activescroll({
+  scroll: "scroll",
+  active: "active",
+  offset: 20,
+  animate: 1000
+});
